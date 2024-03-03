815311187ee7c6571ea8a4cd49d80fdcfde16978
Lucas Mariano <lucasmariano2000@hotmail.com>
2024-03-03 00:44:36 -0300

    feat: "Drawer Menu animations, created new Hook"

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,11 @@
+'use client'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  font-size: 2rem;
+  z-index: 0;
+`
